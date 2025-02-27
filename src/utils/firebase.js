0a7d78dc57c2e50c8ff5a8621160739451e22b47
hasanihardik <hasanihardik@gmail.com>
2025-02-27 14:43:31 +0530

    update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,7 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	apiKey: process.env.REACT_APP_FIREBASE_API,
 	authDomain: "netflickgpt-ef49c.firebaseapp.com",
 	projectId: "netflickgpt-ef49c",
 	storageBucket: "netflickgpt-ef49c.firebasestorage.app",
--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 // Initialize the Google Generative AI with your API key
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY)
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API)
 
 // Create a function to get the model
 const getGeminiModel = () => {
